5ac4aedcb5555e504d6ed350d5a1a80e323fdb19
Pavel Vasilev <p_vasiliev@mail.ru>
2015-09-28 01:48:44 +0300

    add some descriptive comments around variables, functions and the logic

--- run_analysis.R
+++ run_analysis.R
@@ -11,32 +11,39 @@
 # main function to run the analysis:
 run_analysis <- function() {
   setwd("~/dev/data-science/get-data/coursera-getdata/")
-  ensure_data_folder_exists()
   
+  # prepare the working directory with the raw data:
+  ensure_data_folder_exists()  
   zipFile <- "./data/getdata-projectfiles-UCI HAR Dataset.zip"
   ensure_zip_file_is_present(zipFile)
-  
   unpack_zip_file(zipFile)
   
+  # read features and activities (to cover AC3 and AC4):
   features <- read.table('./data/UCI HAR Dataset/features.txt', col.names=c('feature_id', 'feature_name'))
-  activities <- read.table('./data/UCI HAR Dataset/activity_labels.txt',col.names=c('activity_id', 'activity_name'))
+  activities <- read.table('./data/UCI HAR Dataset/activity_labels.txt', col.names=c('activity_id', 'activity_name'))
   
+  # merge train and test data into one (to cover AC1):
   merged_data <- merge_train_and_test_data(features, activities)
-    
+  
+  # figure out what columns are needed (to cover AC2):
   required_columns <- get_required_columns(merged_data)  
   data_means_and_std <- select(merged_data, required_columns)
   
-  data_summarized_by_activity_and_subject <- group_by(data_means_and_std, activity_name, subject_id) %>% summarize_each(funs(mean))
-  
+  # write independent data set (to conver AC5):
+  data_summarized_by_activity_and_subject <- group_by(data_means_and_std, activity_name, subject_id) %>% summarize_each(funs(mean))  
   write.table(data_summarized_by_activity_and_subject, file="./data/tidy_dataset.txt", row.name=FALSE)
 }
 
+
+# ensure ./data dir exists
 ensure_data_folder_exists <- function() {
   if (!file.exists("./data")) {
     dir.create("data")
   }
 }
 
+
+# ensure zip file is downloaded
 ensure_zip_file_is_present <- function(zipFile) {
   if (!file.exists(zipFile)) {
     fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
@@ -44,12 +51,17 @@ ensure_zip_file_is_present <- function(zipFile) {
   }
 }
 
+
+# unpack zip file if needed
 unpack_zip_file <- function(zipFile) {
   if (!file.exists("./data/UCI HAR Dataset")) {
     unzip(zipfile = zipFile, exdir = "./data")
   }
 }
 
+
+# read two sets of data: train and test;
+# here is all the magic to merge to data sets (rbind) and merge with activities
 merge_train_and_test_data <- function(features, activities) {
   x_train <- read.table('./data/UCI HAR Dataset/train/X_train.txt', col.names=features$feature_name)
   y_train <- read.table('./data/UCI HAR Dataset/train/y_train.txt', col.names='activity_id')
@@ -65,6 +77,8 @@ merge_train_and_test_data <- function(features, activities) {
   merge(activities, train_and_test_data_bound)
 }
 
+
+# to cover AC2: extracts only the measurements on the mean and standard deviation for each measurement. 
 get_required_columns <- function(merged_data) {
   pattern_mean <- '*ean*'
   pattern_std <- '*std*'
